bf007a05afe49920bc8501a237e502d502ab7e90
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2025-08-22 22:09:30 -0300

    Update lista1.c

--- lista1.c
+++ lista1.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+
 void questao1() {
+    double salario;
+    // digite o salario
+    scanf("%lf", &salario);
+    // salario = R$ x
+
+    double qtdkW;
+    //digite a quantidade de kW
+    scanf("%lf", &qtdkW);
+    // qtdkW = y kW
+
+    double custokW = (7*salario/1000);
+    double custoConsumo = (7*salario*qtdkW/1000);
+    double custoDesconto = (0.9*7*salario*qtdkW/1000);
+
+    printf("Custo por kW: R$ %.2lf\n", custokW);
+    printf("Custo do consumo: R$ %.2lf\n", custoConsumo);
+    printf("Custo com desconto: R$ %.2lf", custoDesconto);
+}
+
+void questao6() {
         int a, b;
 
     // digite o a
@@ -16,8 +37,9 @@ void questao1() {
 }
 
 int main() {
-
-    questao1();
+        
+        questao1();
+        questao6();
 
     return 0;
 }
