2888d30b770c09d089831fce108999b3301c2009
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2025-08-29 20:28:56 -0300

    Update e7.c

--- e7.c
+++ e7.c
@@ -6,13 +6,13 @@ int main() {
     scanf("%d %d", &numero, &repeticoes);
 
     if((numero % 2) == 0) {
+        printf("%d ", numero);
+        
         for(repeticoes; repeticoes > 1; repeticoes--) {
-            
+            numero += 2;
+            printf("%d ", numero);
         }
-
-
-
-
+        printf("\n");
     } else {
         printf("O PRIMEIRO NUMERO NAO E PAR\n");
     }
